0ca83badfe7507913f7c2ba2bf1e03e1b8934326
melvin.sorum <melvin.sorum@itggot.se>
2018-01-25 10:49:30 +0100

    Del av inloggningssystemet

--- app.rb
+++ app.rb
@@ -1,12 +1,25 @@
 class App < Sinatra::Base
 
-	get '/' do
+	get('/') do
 		'Hemskärm!'
 		slim(:home)
 	end
 	
-	get '/login' do
+	get('/login') do
 		slim(:login)
+	end
+
+	post('login') do
+		db = SQLite3::Database.new('db/webshop.sqlite')
+		username = params["username"]
+		password = params["password"]
+
+		result = db.execute("SELECT id, password_digest FROM user WHERE username=?", [username])
+
+		if result.empty?
+			
+			
+		end
 	end
 end           
 
